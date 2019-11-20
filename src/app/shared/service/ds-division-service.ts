3b410666e8f713cf750107640e6f36e5918f32c7
aruna_zincat <aruna.zincat@gmail.com>
2019-11-20 11:51:25 +0530

    BASE_URL added

--- src/app/shared/service/ds-division-service.ts
+++ src/app/shared/service/ds-division-service.ts
@@ -2,10 +2,11 @@ import {Injectable} from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {GnDivision} from '../dto/gn-division';
 import {Observable} from 'rxjs';
+import { SysConfigService } from './sys-config.service';
 
 @Injectable()
 export class DsDivisionService {
-  public BASE_URL = 'http://localhost:9292/api/dsDivision/';
+  public BASE_URL = SysConfigService.BASE_URL+'/dsDivision/';
   private headers;
   private headersJson = new HttpHeaders({'Content-Type': 'application/json; charset=utf-8'});
   private gnDivision: GnDivision;
