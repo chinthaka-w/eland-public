5f9fc642ca41fc4205de360df42aceb6d23daab7
Ishani Wijesekara <wijesekaraishani23@gmail.com>
2019-11-26 15:58:11 +0530

    View Notary Remark Notary History

--- src/app/public-portal/dashboard/view-notary/notary-remark/remrk-table/remrk-table.component.ts
+++ src/app/public-portal/dashboard/view-notary/notary-remark/remrk-table/remrk-table.component.ts
@@ -3,14 +3,31 @@ import {NewNotaryRequestsCategorySearchDto} from "../../../../../shared/dto/new-
 import {NotaryRegistrationHistoryDto} from "../../../../../shared/dto/notary-registration-history.dto";
 import {ActivatedRoute} from "@angular/router";
 import {NewNotaryDataVarificationService} from "../../../../../shared/service/new-notary-data-varification.service";
+import {animate, state, style, transition, trigger} from "@angular/animations";
+import {MatTableDataSource} from "@angular/material/table";
+import {NewNotaryPaymentDetailDto} from "../../../../../shared/dto/new-notary-payment-detail.dto";
 
 @Component({
   selector: 'app-remrk-table',
   templateUrl: './remrk-table.component.html',
-  styleUrls: ['./remrk-table.component.css']
+  styleUrls: ['./remrk-table.component.css'],
+  animations: [
+    trigger("detailExpand", [
+      state("collapsed", style({ height: "0px", minHeight: "0" })),
+      state("expanded", style({ height: "*" })),
+      transition(
+        "expanded <=> collapsed",
+        animate("225ms cubic-bezier(0.4, 0.0, 0.2, 1)")
+      )
+    ])
+  ]
 })
 export class RemrkTableComponent implements OnInit {
   notaryRequestHistory: NotaryRegistrationHistoryDto[] = [];
+
+  displayedColumns: string[] = ['Workflow Stage', 'Remark', 'Created Time', 'Created User'];
+  dataSource = new MatTableDataSource<NotaryRegistrationHistoryDto>(this.notaryRequestHistory);
+
 
   constructor(private notaryService: NewNotaryDataVarificationService,
               private route: ActivatedRoute) { }
@@ -27,7 +44,7 @@ export class RemrkTableComponent implements OnInit {
 
     this.notaryService.getHistoryDetails(searchType).subscribe(
       (result: NotaryRegistrationHistoryDto[]) => {
-        this.notaryRequestHistory = result;
+        this.dataSource.data = result;
       },
       error1 => {
         console.log(error1);
