211954535277a69eb83fd625e7e508f3bc5c02c2
aruna_zincat <aruna.zincat@gmail.com>
2019-11-29 14:02:09 +0530

    session service added

--- src/app/public-portal/dashboard/dashboard.component.ts
+++ src/app/public-portal/dashboard/dashboard.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { PublicProfileEditComponent } from './profile/public-profile-edit/public-profile-edit.component';
+import { SessionService } from 'src/app/shared/service/session.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -11,10 +12,14 @@ export class DashboardComponent implements OnInit {
 
   // userType = 'notary';
   userType = 'public';
+  user;
 
-  constructor() {}
+  constructor(
+    private sessionService: SessionService
+  ) {}
 
   ngOnInit() {
+    this.user = this.sessionService.getUser();
   }
 
 }
