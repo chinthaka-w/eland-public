cf4dd999a79d003f383da79f7c16b20ecdbafac4
Ishani Wijesekara <wijesekaraishani23@gmail.com>
2019-12-16 15:38:11 +0530

    Name Change Request Service Update

--- src/app/shared/service/change-name.service.ts
+++ src/app/shared/service/change-name.service.ts
@@ -7,22 +7,24 @@ import {Observable} from "rxjs";
   providedIn: 'root'
 })
 export class ChangeNameService {
-  public BASE_URL = SysConfigService.BASE_URL;
+  public BASE_URL = SysConfigService.BASE_URL +'name-change';
   private headers;
   private headersJson = new HttpHeaders({'Content-Type': 'application/json; charset=utf-8'});
 
   constructor(private httpClient: HttpClient) {
   }
-
+  /** get all Name Change Request */
   getNameChangeRequest(notaryId: number): Observable<any> {
-    return this.httpClient.post(this.BASE_URL + 'name-change/changeRequest/' , notaryId, {headers: this.headers} );
+    return this.httpClient.post(this.BASE_URL + '/changeRequest/' , notaryId, {headers: this.headers} );
   }
 
+  /** get Name Change Request by requestId */
   getNameChangeRequestData(requestId: number): Observable<any> {
-    return this.httpClient.get(this.BASE_URL + 'name-change/getRequestDetails/' + requestId.toString() , {headers: this.headers});
+    return this.httpClient.get(this.BASE_URL + '/getRequestDetails/' + requestId.toString() , {headers: this.headers});
   }
 
+  /** save Name Change Request */
   save(formData: FormData): Observable<any> {
-    return this.httpClient.post(this.BASE_URL + 'name-change/submitChangeRequest/' , formData );
+    return this.httpClient.post(this.BASE_URL + '/submitChangeRequest/' , formData );
   }
 }
