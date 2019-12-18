a33caf4b9079a8636d48f3cfa4cddeb9ca8e452a
Sewwandi Gunawardana <sewwandi_g@epiclanka.net>
2019-12-18 08:52:18 +0530

    judicial change request view

--- src/app/public-portal/notary-registration/add-notary/add-notary.component.ts
+++ src/app/public-portal/notary-registration/add-notary/add-notary.component.ts
@@ -211,7 +211,8 @@ export class AddNotaryComponent implements OnInit {
       this.notaryForm.value.englishNameWithInitials,   this.notaryForm.value.fullNameInSinhala, this.notaryForm.value.fullNameInTamil,
       this.notaryForm.value.title, 'Miss', 'Ms',
       this.notaryForm.value.courtZone, this.notaryForm.value.landRegistry, this.dsGnList, this.notaryForm.value.languages,
-      this.notaryForm.value.enrolledDate, this.notaryForm.value.passedDate, this.notaryForm.value.medium, 'status', new Date(), this.notaryForm.value.userName, WorkflowStages.REGISTRATION_REQ_INITIALIZED, this.notaryForm.value.userName,this.paymentDataValue);
+      this.notaryForm.value.enrolledDate, this.notaryForm.value.passedDate, this.notaryForm.value.medium, 'status', new Date(),
+      this.notaryForm.value.userName, WorkflowStages.REGISTRATION_REQ_INITIALIZED, this.notaryForm.value.userName,this.paymentDataValue,null,null);
 
       const formData = new FormData();
       formData.append('data', JSON.stringify(this.notaryDetails));
