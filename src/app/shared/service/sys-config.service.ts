4d545c21ee701e1f5ef532d87973ff15d03ff31a
chinthaka_w <chinthaka_w@epiclanka.net>
2020-02-24 14:56:08 +0530

    Merge branch 'dev' of https://bitbucket.org/ELTPL/eland-public2 into feature/search-workflow
    # Conflicts:
    #	src/app/public-portal/dashboard/extract/extract-view/extract-application/extract-application.component.ts
    #	src/app/public-portal/dashboard/search-document/search-document-view/search-document-application/search-document-application.component.ts
    #	src/app/public-portal/dashboard/view-notary/notary-payment-info/notary-payment-info.component.ts
    #	src/app/shared/enum/parameters.enum.ts

--- src/app/shared/service/sys-config.service.ts
+++ src/app/shared/service/sys-config.service.ts
@@ -9,7 +9,7 @@ export class SysConfigService {
   public static APP_ENDPOINT = "api/";
   public static APP_IP = "localhost:";
   public static APP_PROTOCOL = "http://";
-  public static APP_PORT = "9292/";
+  public static APP_PORT = "9191/";
   // online payment configs
   public static LGPS_PAYMENT_URL = 'https://testlgps.lankagate.gov.lk:9443/lgps/accesslgps?clientPaymentRequest=';
   public static LGPS_SERVICE_CODE = 'TEST10001';
