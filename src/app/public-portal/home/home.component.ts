e5aa9dfd0e64f56a74bdc96c4ff281b5150243e1
aruna_zincat <aruna.zincat@gmail.com>
2020-04-08 10:37:11 +0530

    remove unnessasy codes

--- src/app/public-portal/home/home.component.ts
+++ src/app/public-portal/home/home.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { SysConfigService } from 'src/app/shared/service/sys-config.service';
-import { TranslateService } from '@ngx-translate/core';
-import { Languages } from 'src/app/shared/enum/languages.enum';
 
 @Component({
   selector: 'app-home',
@@ -12,52 +10,18 @@ export class HomeComponent implements OnInit {
 
   status: boolean = false;
   dropdown: boolean = false;
-  langDropdown: boolean = false;
-
-  public languages = [
-    {
-        id   : Languages.ENGLISH,
-        label: 'English',
-        code: 'en'
-    },
-    {
-        id   : Languages.SINHALA,
-        label: 'සිංහල',
-        code: 'si'
-    },
-    {
-        id   : Languages.TAMIL,
-        label: 'தமிழ்',
-        code: 'ta'
-    }
-];
-
-public selectedLanguage = this.languages[0];
 
   clickEvent() {
     this.status = !this.status;
     this.dropdown = false;
   }
 
-  constructor(
-    public translate: TranslateService
-  ) {
-    
+  constructor() {
   }
 
   ngOnInit() {
 
-  }
 
-
-  changeLanguage(lang){
-    this.translate.use(lang.code);
-    this.selectedLanguage = lang;
-    this.clickLangDropdown();
-  }
-
-  clickLangDropdown(){
-    this.langDropdown = !this.langDropdown;
   }
 
 }
