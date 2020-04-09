884e013be70389aab12f3deb163cf91472ef56e7
erandakaanupama <erandakaanupama@gmail.com>
2020-04-09 21:53:12 +0530

    status enum update

--- src/app/shared/enum/common-status.enum.ts
+++ src/app/shared/enum/common-status.enum.ts
@@ -5,10 +5,6 @@ export enum CommonStatus {
   SUCCESS = 'SUCCESS',
   FAIL = 'FAIL',
   REQUIRED = 'REQUIRED',
-<<<<<<< HEAD
   OPTIONAL = 'OPTIONAL',
   REJECT = 'REJ'
-=======
-  OPTIONAL = 'OPTIONAL'
->>>>>>> 13cd9b5e93310d1cb63d8504f4b4f087298b99ea
 }
