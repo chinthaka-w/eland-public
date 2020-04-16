497f4618d1b352af7ed92929a2808b3db75e20e4
chinthaka_w <chinthaka_w@epiclanka.net>
2020-04-16 19:41:15 +0530

    bug fixing

--- src/app/public-portal/dashboard/view-notary/notary-payment-info/notary-payment-info.component.ts
+++ src/app/public-portal/dashboard/view-notary/notary-payment-info/notary-payment-info.component.ts
@@ -67,7 +67,9 @@ export class NotaryPaymentInfoComponent implements OnInit {
 
 
   getPaymentDetails() {
-    let searchType: NewNotaryRequestsCategorySearchDto = new NewNotaryRequestsCategorySearchDto(this.requestDetailPayment !== undefined ? this.requestDetailPayment.requestId : this.id, this.requestDetailPayment !== undefined ? this.requestDetailPayment.workflow : this.workflow);
+    let searchType: NewNotaryRequestsCategorySearchDto = new NewNotaryRequestsCategorySearchDto(
+      this.requestDetailPayment !== undefined ? this.requestDetailPayment.requestId : this.id,
+      this.requestDetailPayment !== undefined ? Workflow.NOTARY_REGISTRATION : this.workflow);
     this.notaryService.getPaymentDetails(searchType).subscribe(
       (result: NewNotaryPaymentDetailDto[]) => {
         this.paymentDetails = result;
