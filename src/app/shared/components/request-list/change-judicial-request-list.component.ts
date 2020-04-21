d9475bc413cc79ed1c16e8288e2837385d45210e
lahiru_b <lahiru_b@epiclanka.net>
2020-04-21 17:31:04 +0530

    name change

--- src/app/shared/components/request-list/change-judicial-request-list.component.ts
+++ src/app/shared/components/request-list/change-judicial-request-list.component.ts
@@ -2,7 +2,7 @@ import { RequestResponse } from './../../dto/request-response.model';
 import { CorrectionRequestService } from './../../service/correction-request.service';
 import {LanguageRequest} from './../../dto/language-request.model';
 import {LanguageChangeService} from './../../service/language-change.service';
-import {Component, OnInit, ViewChild} from '@angular/core';
+import {ChangeDetectorRef, Component, OnInit, ViewChild} from '@angular/core';
 import {MatPaginator, MatSort, MatTableDataSource} from '@angular/material';
 import {JudicialChange} from '../../dto/judicial-change-model';
 import {SnackBarService} from '../../service/snack-bar.service';
@@ -19,6 +19,8 @@ import {ChangeNameService} from '../../service/change-name.service';
 import {NotaryNameChangeModel} from '../../dto/notary-name-change.model';
 import {ChangeLandRegistryService} from '../../service/change-land-registry.service';
 import {ChangeLandRegistryDto} from '../../dto/change-land-registry.dto';
+import {SystemService} from '../../service/system.service';
+import {LangChangeEvent, TranslateService} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-change-judicial-request-list',
@@ -42,6 +44,7 @@ export class ChangeJudicialRequestListComponent implements OnInit {
   public headerText: string;
   public newButtonURL: string;
   public actionButtonURL: string;
+  public currentLang: string;
 
   previousUrl = '';
 
@@ -56,13 +59,66 @@ export class ChangeJudicialRequestListComponent implements OnInit {
               private tokenStorageService: TokenStorageService,
               private langChangeService: LanguageChangeService,
               private landregistryService: ChangeLandRegistryService,
-              private folioCorrectionService: CorrectionRequestService) {
+              private folioCorrectionService: CorrectionRequestService,
+              private systemService: SystemService,
+              private translate: TranslateService,
+              private changeDetectorRef: ChangeDetectorRef) {
     this.activatedRoute.params.subscribe(params => {
-      this.flag = atob(params['flag']); // (+) converts string 'id' to a number
+      this.flag = atob(params.flag); // (+) converts string 'id' to a number
     });
 
     this.previousUrl = this.router.url;
-  }
+
+    // translate.onLangChange.subscribe((event: LangChangeEvent) => {
+    //   this.currentLang = event.lang;
+    //   if (this.flag === Workflow.CHANGE_LAND_REGISTRY) {
+    //     //alert("methana");
+    //     this.loadData();
+    //     this.headerText = this.translate.instant('PUBLIC_COMMON.JUD_TITLE1');
+    //     this.titleText = this.translate.instant('PUBLIC_COMMON.JUD_TITLE2');
+    //   }
+    //   else if (this.flag === Workflow.SEARCH_REQUEST) {
+    //    // alert("athana" + this.translate.instant('PUBLIC_COMMON.SEARCH_TITLE2'));
+    //     this.headerText = this.translate.instant('PUBLIC_COMMON.SEARCH_TITLE1');
+    //     this.titleText = this.translate.instant('PUBLIC_COMMON.SEARCH_TITLE2');
+    //   }
+    //   //this._changeDetectorRef.detectChanges();
+    // });
+    translate.onLangChange.subscribe((event: LangChangeEvent) => {
+      this.currentLang = event.lang;
+    switch (this.flag) {
+      case Workflow.JUDICIAL_ZONE_CHANGE:
+        this.headerText = this.translate.instant('PUBLIC_COMMON.JUD_TITLE1');
+        this.titleText = this.translate.instant('PUBLIC_COMMON.JUD_TITLE2');
+        break;
+      case Workflow.NOTARY_NAME_CHANGE:
+        this.headerText = this.translate.instant('PUBLIC_COMMON.NOT_TITEL1');
+        this.titleText = this.translate.instant('PUBLIC_COMMON.NOT_TITLE2');
+        break;
+      case Workflow.SEARCH_REQUEST:
+        this.headerText = this.translate.instant('PUBLIC_COMMON.SEARCH_TITLE1');
+        this.titleText = this.translate.instant('PUBLIC_COMMON.SEARCH_TITLE2');
+        break;
+      case Workflow.EXTRACT_REQUEST:
+        this.headerText = this.translate.instant('PUBLIC_COMMON.EXTRACT_TIT1');
+        this.titleText = this.translate.instant('PUBLIC_COMMON.EXTRACT_TIT2');
+        break;
+      case Workflow.LANGUAGE_CHANGE:
+        this.headerText = this.translate.instant('PUBLIC_COMMON.LANG1');
+        this.titleText = this.translate.instant('PUBLIC_COMMON.LANG2');
+        break;
+      case Workflow.CHANGE_LAND_REGISTRY:
+        this.headerText = this.translate.instant('PUBLIC_COMMON.REGISTRY1');
+        this.titleText = this.translate.instant('PUBLIC_COMMON.REGISTRY2');
+        break;
+
+      case Workflow.FOLIO_REQUEST_CORRECTION:
+        this.headerText = this.translate.instant('PUBLIC_COMMON.SECTION351');
+        this.titleText = this.translate.instant('PUBLIC_COMMON.SECTION352');
+        break;
+    }
+    });
+    }
 
   ngOnInit() {
     this.loadData();
@@ -72,51 +128,51 @@ export class ChangeJudicialRequestListComponent implements OnInit {
     switch (this.flag) {
       case Workflow.JUDICIAL_ZONE_CHANGE:
         this.loadJudicialChangeRequests();
-        this.headerText = 'JUDICIAL ZONE CHANGING';
-        this.titleText = 'REQUEST FOR CHANGING THE JUDICIAL ZONE';
+        this.headerText = this.translate.instant('PUBLIC_COMMON.JUD_TITLE1');
+        this.titleText = this.translate.instant('PUBLIC_COMMON.JUD_TITLE2');
         this.newButtonURL = '/change-judicial';
         this.actionButtonURL = '/change-judicial-request-view/';
         break;
       case Workflow.NOTARY_NAME_CHANGE:
         this.loadNameChangeRequests();
-        this.headerText = 'NOTARY NAME CHANGING';
-        this.titleText = 'REQUEST FOR CHANGING THE NOTARY NAME';
+        this.headerText = this.translate.instant('PUBLIC_COMMON.NOT_TITEL1');
+        this.titleText = this.translate.instant('PUBLIC_COMMON.NOT_TITLE2');
         this.newButtonURL = '/change-the-name';
         this.actionButtonURL = `/change-name-request-view/`;
         break;
       case Workflow.SEARCH_REQUEST:
         this.loadSearchRequests();
-        this.headerText = 'FOLIO / DEED SEARCH';
-        this.titleText = 'REQUEST FOR SEARCH DOCUMENT';
+        this.headerText = this.translate.instant('PUBLIC_COMMON.SEARCH_TITLE1');
+        this.titleText = this.translate.instant('PUBLIC_COMMON.SEARCH_TITLE2');
         this.newButtonURL = '/search-document';
         this.actionButtonURL = `/search-document-view/`;
         break;
       case Workflow.EXTRACT_REQUEST:
         this.loadExtractRequests();
-        this.headerText = 'FOLIO / DEED EXTRACT';
-        this.titleText = 'REQUEST FOR EXTRACT DOCUMENT';
+        this.headerText = this.translate.instant('PUBLIC_COMMON.SEARCH_TITLE1');
+        this.titleText = this.translate.instant('PUBLIC_COMMON.SEARCH_TITLE2');
         this.newButtonURL = '/extract';
         this.actionButtonURL = `/extract-view/`;
         break;
       case Workflow.LANGUAGE_CHANGE:
         this.loadLanguageChangeRequests();
-        this.headerText = 'LANGUAGE CHANGE';
-        this.titleText = 'REQUEST FOR LANGUAGE CHANGE';
+        this.headerText = this.translate.instant('PUBLIC_COMMON.REGISTRY1');
+        this.titleText = this.translate.instant('PUBLIC_COMMON.REGISTRY2');
         this.newButtonURL = '/language-change';
         this.actionButtonURL = '/language-change-view/';
         break;
       case Workflow.CHANGE_LAND_REGISTRY:
         this.loadLandRegistryRequests();
-        this.headerText = 'LAND REGISTRY CHANGE';
-        this.titleText = 'REQUEST FOR LAND REGISTRY CHANGE';
+        this.headerText = this.translate.instant('PUBLIC_COMMON.JUD_TITLE1');
+        this.titleText = this.translate.instant('PUBLIC_COMMON.JUD_TITLE2');
         this.newButtonURL = '/change-registry';
         this.actionButtonURL = '/change-land-registry-view/';
         break;
       // section 35 corrections
       case Workflow.FOLIO_REQUEST_CORRECTION:
         this.getFolioCorrctionRequest();
-        this.headerText = 'Section 35 Correction';
-        this.titleText = 'REQUEST FOR Section 35 Correction';
+        this.headerText = this.translate.instant('PUBLIC_COMMON.SECTION351');
+        this.titleText = this.translate.instant('PUBLIC_COMMON.SECTION352');
         this.newButtonURL = '/request-for-correction';
         this.actionButtonURL = '/request-for-correction/';
         break;
@@ -212,19 +268,19 @@ export class ChangeJudicialRequestListComponent implements OnInit {
     );
   }
 
-/**
- * Get all land registry requests
- */
+  /**
+   * Get all land registry requests
+   */
 
   loadLandRegistryRequests() {
     this.landregistryService.getAllRequests(this.sessionService.getUser().id).subscribe(
       (result: ChangeLandRegistryDto[]) => {
-      this.dataSource = new MatTableDataSource(result);
-      this.dataSource.paginator = this.paginator;
-      this.dataSource.sort = this.sort;
-    });
+        this.dataSource = new MatTableDataSource(result);
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort;
+      });
 
-}
+  }
 
 
   getFolioCorrctionRequest() {
