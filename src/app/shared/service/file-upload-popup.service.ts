cae58f385034721cf088443911715d2e8bfce58e
erandakaanupama <erandakaanupama@gmail.com>
2020-04-23 09:48:23 +0530

    doc update service added

--- src/app/shared/service/file-upload-popup.service.ts
+++ src/app/shared/service/file-upload-popup.service.ts
@@ -1,8 +1,10 @@
+import { FileMeta } from './../dto/file-meta.model';
 import { WorkflowStageDocTypeDTO } from './../dto/workflow-stage-doc-type-dto';
 import { Observable } from 'rxjs';
 import { SysConfigService } from './sys-config.service';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { RequestResponse } from '../dto/request-response.model';
 
 @Injectable({
   providedIn: 'root'
@@ -15,5 +17,9 @@ export class FileUploadPopupService {
 
   public getWorkflowStageDocuments(workflowStage: string): Observable<WorkflowStageDocTypeDTO[]> {
     return this.http.get<WorkflowStageDocTypeDTO[]>(this.BASE_URL + 'supportingDocument/' + workflowStage);
+  }
+
+  updateDocuments(filesMeta: FileMeta[]): Observable<RequestResponse> {
+    return this.http.post<RequestResponse>(this.BASE_URL + 'document/update', filesMeta);
   }
 }
