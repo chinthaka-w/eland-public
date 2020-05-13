d67bbeb4df68df10ef992bffd33407cc23b6aabf
chinthaka_w <chinthaka_w@epiclanka.net>
2020-05-13 23:56:58 +0530

    new notary bug fixing

--- src/app/shared/dto/workflow-stage-doc.dto.ts
+++ src/app/shared/dto/workflow-stage-doc.dto.ts
@@ -1,6 +1,8 @@
 export class WorkflowStageDocDto {
   public descriptionSin: string;
   public descriptionTam: string;
+  public invalid: boolean;
+
   constructor(
     public docId: number,
     public docTypeId: number,
