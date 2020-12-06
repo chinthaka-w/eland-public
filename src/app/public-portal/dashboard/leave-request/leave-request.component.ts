220b0a5f10497a4a624431075ae9bdab6cf74b63
chinthaka_w <chinthaka_w@epiclanka.net>
2020-12-07 00:20:55 +0530

    35854 bug

--- src/app/public-portal/dashboard/leave-request/leave-request.component.ts
+++ src/app/public-portal/dashboard/leave-request/leave-request.component.ts
@@ -49,7 +49,9 @@ export class LeaveRequestComponent implements OnInit {
     this.notaryLeaveRequestForm = new FormGroup({
       fromDate: new FormControl('', [Validators.required]),
       toDate: new FormControl('', [Validators.required]),
-      reason: new FormControl('', [Validators.required,this.sysMethodsService.noWhitespaceValidator]),
+      reason: new FormControl('', [Validators.required,
+        this.sysMethodsService.noWhitespaceValidator,
+      Validators.maxLength(255)]),
       recaptcha: new FormControl('', [Validators.required])
     });
     this.route.params.subscribe(param => {
